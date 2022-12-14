aeed0bf6bb70748c8c30cfef229f7cf2cf4ef821
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-14 16:06:14 -0300

    feat: método create criado em UserModel

---
+++ src/db/models/user.model.ts
@@ -0,0 +1,23 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import User from '../../interfaces/user.interface';
+
+export default class UserModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public create = async (user: User): Promise<User> => {
+    const { username, vocation, level, password } = user;
+    const query = `INSERT INTO Trybesmith.users (username, vocation, level, password)
+    VALUES (?, ?, ?, ?)`;
+    const [{ insertId }] = await this
+      .connection.execute<ResultSetHeader>(query, [username, vocation, level, password]);
+
+    return {
+      id: insertId,
+      ...user,
+    };
+  };
+}
