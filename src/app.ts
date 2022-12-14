e44b2540e2b6539c3659f0880f174c809c3588f2
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-14 20:06:48 -0300

    feat: login.route criado em caso de sucesso, sem validações ainda

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import productRoutes from './routes/product.routes';
 import UserRoutes from './routes/user.routes';
 import OrderRoutes from './routes/order.routes';
+import LoginRoute from './routes/login.routes';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/products', productRoutes);
 app.use('/users', UserRoutes);
 app.use('/orders', OrderRoutes);
+app.use('/login', LoginRoute);
 
 export default app;
