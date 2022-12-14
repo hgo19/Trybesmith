8df96898eb4b7fa09a800cf82d779ea15a09a6ea
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-14 15:57:35 -0300

    feat: método getAll em product.controllers criado

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import ProductService from '../services/book.services';
+import ProductService from '../services/product.services';
 
 export default class ProductController {
   constructor(private productService = new ProductService()) {}
@@ -8,5 +8,10 @@ export default class ProductController {
     const product = req.body;
     const createdProduct = await this.productService.create(product);
     res.status(201).json(createdProduct);
+  };
+
+  public getAll = async (_req: Request, res: Response) => {
+    const products = await this.productService.getAll();
+    res.status(200).json(products);
   };
 }
