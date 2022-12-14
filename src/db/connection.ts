131be1b74234e4747088a5a3d3117b56a1a27e26
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-14 15:04:45 -0300

    product.model criado e método create adicionado a classe. interface product criada

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -7,5 +7,5 @@ export default mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'trybesmith_db',
+  database: 'Trybesmith',
 });
---
+++ src/db/models/product.model.ts
@@ -0,0 +1,22 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../../interfaces/product.interface';
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public create = async (product: Product): Promise<Product> => {
+    const { name, amount } = product;
+    const query = `INSERT INTO products (name, amount)
+      VALUES (?, ?)`;
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(query, [name, amount]);
+
+    return {
+      id: insertId,
+      ...product,
+    };
+  };
+}
