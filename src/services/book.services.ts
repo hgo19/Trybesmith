578709fc6ee3397e187a4993f4d2c10a71928d50
Hugo Leonardo <huleonardo19@gmail.com>
2022-12-14 15:14:57 -0300

    feat: ProductService criado e método ProductService.create criado

---
+++ src/services/book.services.ts
@@ -0,0 +1,16 @@
+import connection from '../db/connection';
+import ProductModel from '../db/models/product.model';
+import Product from '../interfaces/product.interface';
+
+export default class ProductService {
+  public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public create = async (product: Product): Promise<Product> => {
+    const createdProduct = await this.model.create(product);
+    return createdProduct;
+  };
+}
